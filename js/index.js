efb04a606bad31e3b3cc4d6f889f49e4cb8a51f2
hossamkhalid <hossamkhalid@gmail.com>
2015-12-07 14:30:48 +0000

    Fix file api

--- js/index.js
+++ js/index.js
@@ -34,70 +34,6 @@ var app = {
     // function, we must explicitly call 'app.receivedEvent(...);'
     onDeviceReady: function() {
         app.receivedEvent('deviceready');
-		
-		function readFromFile(fileName) {
-			var pathToFile = cordova.file.dataDirectory + fileName;
-			window.resolveLocalFileSystemURL(pathToFile, function (fileEntry) {
-				fileEntry.file(function (file) {
-					var reader = new FileReader();
-
-					reader.onloadend = function (e) {
-						return JSON.parse(this.result);
-					};
-
-					reader.readAsText(file);
-				}, errorHandler.bind(null, fileName));
-			}, errorHandler.bind(null, fileName));
-		}
-		
-		function writeToFile(fileName, data) {
-			data = JSON.stringify(data, null, '\t');
-			window.resolveLocalFileSystemURL(cordova.file.dataDirectory, function (directoryEntry) {
-				directoryEntry.getFile(fileName, { create: true }, function (fileEntry) {
-					fileEntry.createWriter(function (fileWriter) {
-						fileWriter.onwriteend = function (e) {
-							// for real-world usage, you might consider passing a success callback
-							console.log('Write of file "' + fileName + '"" completed.');
-						};
-
-						fileWriter.onerror = function (e) {
-							// you could hook this up with our global error handler, or pass in an error callback
-							console.log('Write failed: ' + e.toString());
-						};
-
-						var blob = new Blob([data], { type: 'text/plain' });
-						fileWriter.write(blob);
-					}, errorHandler.bind(null, fileName));
-				}, errorHandler.bind(null, fileName));
-			}, errorHandler.bind(null, fileName));
-		}
-		
-		var errorHandler = function (fileName, e) {  
-			var msg = '';
-
-			switch (e.code) {
-				case FileError.QUOTA_EXCEEDED_ERR:
-					msg = 'Storage quota exceeded';
-					break;
-				case FileError.NOT_FOUND_ERR:
-					msg = 'File not found';
-					break;
-				case FileError.SECURITY_ERR:
-					msg = 'Security error';
-					break;
-				case FileError.INVALID_MODIFICATION_ERR:
-					msg = 'Invalid modification';
-					break;
-				case FileError.INVALID_STATE_ERR:
-					msg = 'Invalid state';
-					break;
-				default:
-					msg = 'Unknown error';
-					break;
-			};
-
-			console.log('Error (' + fileName + '): ' + msg);
-		}
     },
     // Update DOM on a Received Event
     receivedEvent: function(id) {
@@ -115,6 +51,70 @@ var app = {
 
 var storedCards = [];
 var currentCard = {};
+
+function readFromFile(fileName) {
+	var pathToFile = cordova.file.dataDirectory + fileName;
+	window.resolveLocalFileSystemURL(pathToFile, function (fileEntry) {
+		fileEntry.file(function (file) {
+			var reader = new FileReader();
+
+			reader.onloadend = function (e) {
+				return JSON.parse(this.result);
+			};
+
+			reader.readAsText(file);
+		}, errorHandler.bind(null, fileName));
+	}, errorHandler.bind(null, fileName));
+}
+
+function writeToFile(fileName, data) {
+	data = JSON.stringify(data, null, '\t');
+	window.resolveLocalFileSystemURL(cordova.file.dataDirectory, function (directoryEntry) {
+		directoryEntry.getFile(fileName, { create: true }, function (fileEntry) {
+			fileEntry.createWriter(function (fileWriter) {
+				fileWriter.onwriteend = function (e) {
+					// for real-world usage, you might consider passing a success callback
+					console.log('Write of file "' + fileName + '"" completed.');
+				};
+
+				fileWriter.onerror = function (e) {
+					// you could hook this up with our global error handler, or pass in an error callback
+					console.log('Write failed: ' + e.toString());
+				};
+
+				var blob = new Blob([data], { type: 'text/plain' });
+				fileWriter.write(blob);
+			}, errorHandler.bind(null, fileName));
+		}, errorHandler.bind(null, fileName));
+	}, errorHandler.bind(null, fileName));
+}
+
+var errorHandler = function (fileName, e) {  
+	var msg = '';
+
+	switch (e.code) {
+		case FileError.QUOTA_EXCEEDED_ERR:
+			msg = 'Storage quota exceeded';
+			break;
+		case FileError.NOT_FOUND_ERR:
+			msg = 'File not found';
+			break;
+		case FileError.SECURITY_ERR:
+			msg = 'Security error';
+			break;
+		case FileError.INVALID_MODIFICATION_ERR:
+			msg = 'Invalid modification';
+			break;
+		case FileError.INVALID_STATE_ERR:
+			msg = 'Invalid state';
+			break;
+		default:
+			msg = 'Unknown error';
+			break;
+	};
+
+	console.log('Error (' + fileName + '): ' + msg);
+}
 
 function scan() {
     var scanner = cordova.require("cordova/plugin/BarcodeScanner");
